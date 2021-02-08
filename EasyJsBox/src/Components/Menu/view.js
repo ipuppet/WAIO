fabe00ecafd7ad07b00778f965521c48626fba93
子明 <37722993+ipuppet@users.noreply.github.com>
2021-02-08 22:18:44 +0800

    更新框架

--- EasyJsBox/src/Components/Menu/view.js
+++ EasyJsBox/src/Components/Menu/view.js
@@ -4,12 +4,13 @@ class View extends BaseView {
     init() {
         this.dataCenter.set("itemIdPrefix", "menu-item-")
         this.dataCenter.set("id", "menu")
-        this.selected = 0 // 当前菜单
+        // 从Page组件获取当前应该设置的菜单索引
+        this.selected = this.kernel.getComponent("Page").dataCenter.get("selectedPage") // 当前菜单
         this.menuLayout = { // menu layout
             menuItem: (make, view) => {
                 make.size.equalTo(50)
-                let length = this.dataCenter.get("menus").length
-                let spacing = (this.getMenuWidth() - length * 50) / (length + 1)
+                const length = this.dataCenter.get("menus").length
+                const spacing = (this.getMenuWidth() - length * 50) / (length + 1)
                 if (view.prev) {
                     make.left.equalTo(view.prev.right).offset(spacing)
                 } else {
@@ -19,7 +20,7 @@ class View extends BaseView {
             menuBar: (make, view) => {
                 make.centerX.equalTo(view.super)
                 make.width.equalTo(this.getMenuWidth())
-                let isLargeScreen = this.isLargeScreen()
+                const isLargeScreen = this.isLargeScreen()
                 make.top.equalTo(view.super.safeAreaBottom).offset(-50)
                 make.bottom.equalTo(view.super)
                 $("menu").cornerRadius = isLargeScreen ? 10 : 0
@@ -55,7 +56,7 @@ class View extends BaseView {
                 this.dataCenter.get("menus")[i].icon = [this.dataCenter.get("menus")[i].icon[0], this.dataCenter.get("menus")[i].icon[0]]
             }
             // menu模板
-            let menu = {
+            const menu = {
                 info: {
                     index: i,
                     icon: {
